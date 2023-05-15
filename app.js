a4f6abba3d1977b6e8359d2bbdcead88ec4126c8
elviawu <elviawu@gmail.com>
2023-05-16 02:44:41 +0800

    add facebook login and dotenv

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 const express = require('express')
 const session = require('express-session')
-// 載入設定檔，要寫在 express-session 以後
-const usePassport = require('./config/passport') 
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const flash = require('connect-flash')
+
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+// 載入設定檔，要寫在 express-session 以後
 const routes = require('./routes')
+const usePassport = require('./config/passport')
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(session({
-  secret: 'ThisIsMySecret',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }))
